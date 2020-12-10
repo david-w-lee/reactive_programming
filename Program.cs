dc38fd40e38e614570cd23b8b2b0df87d76abbb4
sea0920@gmail.com <sea0920@hotmail.com>
2020-12-09 22:51:12 -0800

    Fifth Commit

--- Program.cs
+++ Program.cs
@@ -11,31 +11,6 @@ namespace reactive_programming
 
     class Program
     {
-
-        private void Test1()
-        {
-            IObservable<string> obj = Observable.Generate(0, _ => true, i => i + 1, i => new string('#', i), i => TimeSelector(i));
-
-            using (obj.Subscribe(Console.WriteLine))
-            {
-                Console.WriteLine("Press any key to exit!!!");
-                Console.ReadLine();
-            }
-
-            Console.WriteLine("Hello World!");
-        }
-
-        public class MyObservable : IObservable<int>
-        {
-            public IDisposable Subscribe(IObserver<int> observer)
-            {
-                observer.OnNext(1);
-                observer.OnNext(2);
-                observer.OnNext(3);
-                return Disposable.Empty;
-            }
-        }
-
         public class MyObserver : IObserver<int>
         {
             public void OnCompleted()
@@ -54,9 +29,12 @@ namespace reactive_programming
             }
         }
 
-        private void Test2()
+
+        /// <summary>
+        /// In an Observable, we invoke observers as callbacks.
+        /// </summary>
+        private void Test1()
         {
-            //MyObservable observable = new MyObservable();
             IObservable<int> observable = Observable.Create<int>(obsvr =>
             {
                 obsvr.OnNext(1);
@@ -76,7 +54,11 @@ namespace reactive_programming
             Console.WriteLine("Hello World!");
         }
 
-        private void Test3()
+        /// <summary>
+        /// Subject allow us to invoke OnNext(), OnComplete(), OnError().
+        /// This allows us multicast messages to observers.
+        /// </summary>
+        private void Test2()
         {
             Subject<string> messenger = new Subject<string>();
             messenger.Subscribe(msg => { Console.WriteLine($"msg: {msg}"); });
@@ -85,18 +67,48 @@ namespace reactive_programming
             messenger.OnNext("Message 1");
             messenger.OnNext("Message 2");
 
+            // Returns the last message before subscription.
+            BehaviorSubject<string> bmessenger = new BehaviorSubject<string>("start point");
+
+            // Returns all the messages before subscritiption.
+            ReplaySubject<string> rmessenger = new ReplaySubject<string>();
+
+            Console.ReadLine();
+        }
+
+        // BehaviorSubject returns the last message before subscription.
+        private void Test3()
+        {
+            BehaviorSubject<string> messenger = new BehaviorSubject<string>("start point");
+            messenger.Subscribe(msg => { Console.WriteLine($"David received: {msg}"); });
+            
+            messenger.OnNext("Message 1");
+            messenger.OnNext("Message 2");
+
+            messenger.Subscribe(msg => { Console.WriteLine($"Nick received: {msg}"); });
+
+            Console.ReadLine();
+        }
+
+        // ReplaySubject returns all the messages before subscritiption.
+        private void Test4()
+        {
+            ReplaySubject<string> messenger = new ReplaySubject<string>();
+            messenger.Subscribe(msg => { Console.WriteLine($"David received: {msg}"); });
+
+            messenger.OnNext("Message 1");
+            messenger.OnNext("Message 2");
+
+            messenger.Subscribe(msg => { Console.WriteLine($"Nick received: {msg}"); });
+
             Console.ReadLine();
         }
 
         static void Main(string[] args)
         {
             Program p = new Program();
-            p.Test3();
+            p.Test2();
         }
 
-        private static TimeSpan TimeSelector(int i)
-        {
-            return TimeSpan.FromSeconds(i);
-        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Reactive Programming
+
+
+
+## Summary
+
+* Based on my experience, Subject seems to be the only useful feature in Reactive Programming.
+* Reactive Programming is known to be 100 times slower than normal event-delegate so it can be used to multi-cast non realtime, historical messages. But it doesn't seem to have any use beyond that.
+
+
+
+### Ref
+
+* Here is a very good introduction to Subjects
+  * https://www.freecodecamp.org/news/an-introduction-to-subjects-in-reactive-programming-bbdc8fed7b6/
+
